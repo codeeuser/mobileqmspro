b4ae3be5a0161ca4268f73ce36002cab4af4af02
Wheref <codeeuser@gmail.com>
2024-11-17 10:33:35 +0800

    update web for Running Tokens

--- mobileqmspro_flutter/lib/pages/web_page.dart
+++ mobileqmspro_flutter/lib/pages/web_page.dart
@@ -1,3 +1,5 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:mobileqmspro_client/mobileqmspro_client.dart';
 import 'package:mobileqmspro/commons/no_data.dart';
@@ -17,8 +19,10 @@ class WebPage extends StatefulWidget {
 class _WebPageState extends State<WebPage> {
   static const String tag = "WebPage";
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-
+  final double _boxWidth = 300;
   final ValueNotifier<List<TokenIssued>> _tokenIssuedList = ValueNotifier([]);
+
+  TokenIssued? _latest;
 
   @override
   void initState() {
@@ -45,95 +49,185 @@ class _WebPageState extends State<WebPage> {
             key: _scaffoldKey,
             body: SafeArea(
               top: false,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 4),
-                    Utils.buildDesc(60),
-                    FutureBuilder<QueueWindow?>(
-                        future: client.queueWindow.findById(widget.windowId),
-                        builder: (context, snapshot) {
-                          switch (snapshot.connectionState) {
-                            case ConnectionState.waiting:
-                              return Utils.loadingScreen();
-                            case ConnectionState.active:
-                            case ConnectionState.done:
-                              final window =
-                                  (snapshot.hasData) ? snapshot.data : null;
-                              return Center(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                      window?.name ??
-                                          '${S.of(context).loading}...',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleLarge),
-                                ),
-                              );
-                            default:
-                              return const NoData();
-                          }
-                        }),
-                    ValueListenableBuilder<List<TokenIssued>>(
-                        valueListenable: _tokenIssuedList,
-                        builder: (BuildContext context, tokenIssuedList, _) {
-                          if (tokenIssuedList.isEmpty) {
-                            return const NoData();
-                          }
-                          return Wrap(
-                            children: [
-                              for (var token in tokenIssuedList) ...[
-                                _tokenIssuedItem(token),
-                              ]
-                            ],
-                          );
-                        }),
-                  ],
-                ),
-              ),
+              child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                if (constraints.maxWidth > 1000) {
+                  return Center(
+                      child: SizedBox(width: 1000, child: _buildBigScreen()));
+                } else {
+                  return _buildSmallScreen();
+                }
+              }),
             )));
   }
 
+  Widget _buildBigScreen() {
+    return Column(
+      children: [
+        const SizedBox(height: 4),
+        Utils.buildDesc(60),
+        Expanded(
+          child: ValueListenableBuilder<List<TokenIssued>>(
+              valueListenable: _tokenIssuedList,
+              builder: (context, list, _) {
+                final List<TokenIssued> listClone = List.from(list);
+                listClone
+                    .sort((a, b) => b.modifiedDate!.compareTo(a.modifiedDate!));
+
+                final latest = listClone.firstOrNull;
+                _latest = latest;
+                Logger.log(tag,
+                    message:
+                        'firstID, ${latest?.tokenLetter}-${latest?.tokenNumber}');
+
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildWaitColumn(list),
+                    _buildQueueColumn(list),
+                    _buildCompletedColumn(list),
+                  ],
+                );
+              }),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildWaitColumn(List<TokenIssued> list) {
+    final tokens = list.where((e) => e.isOnWait).toList();
+    return _buildVerticalColumn(tokens, S.of(context).pleaseWait);
+  }
+
+  Widget _buildQueueColumn(List<TokenIssued> list) {
+    final tokens = list.where((e) => e.isOnQueue).toList();
+    return _buildVerticalColumn(tokens, S.of(context).myQueue);
+  }
+
+  Widget _buildCompletedColumn(List<TokenIssued> list) {
+    final tokens = list.where((e) => e.isCompleted).toList();
+    tokens.sort((a, b) => a.statusCode.compareTo(b.statusCode));
+    return _buildVerticalColumn(tokens, S.of(context).completedTokens);
+  }
+
+  Widget _buildVerticalColumn(List<TokenIssued> tokens, String title) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            width: _boxWidth,
+            height: 40,
+            color: Colors.grey,
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+            child: Text(
+              title,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.apply(color: Colors.black),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          for (TokenIssued token in tokens) ...[
+            _tokenIssuedItem(token),
+          ]
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSmallScreen() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 4),
+          Utils.buildDesc(60),
+          FutureBuilder<QueueWindow?>(
+              future: client.queueWindow.findById(widget.windowId),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return Utils.loadingScreen();
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    final window = (snapshot.hasData) ? snapshot.data : null;
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                            window?.name ?? '${S.of(context).loading}...',
+                            style: Theme.of(context).textTheme.titleLarge),
+                      ),
+                    );
+                  default:
+                    return const NoData();
+                }
+              }),
+          ValueListenableBuilder<List<TokenIssued>>(
+              valueListenable: _tokenIssuedList,
+              builder: (BuildContext context, tokenIssuedList, _) {
+                if (tokenIssuedList.isEmpty) {
+                  return const NoData();
+                }
+                return Wrap(
+                  children: [
+                    for (var token in tokenIssuedList) ...[
+                      _tokenIssuedItem(token),
+                    ]
+                  ],
+                );
+              }),
+        ],
+      ),
+    );
+  }
+
   Widget _tokenIssuedItem(TokenIssued tokenIssued) {
+    bool animation = (tokenIssued.id == _latest?.id);
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width: 300,
-        height: 160,
-        color: Utils.getStatusColor(tokenIssued.statusName),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  boxShadow: const [
-                    BoxShadow(color: Colors.white, spreadRadius: 1),
-                  ],
+      child: Pulse(
+        key: ValueKey('token-${tokenIssued.id}'),
+        duration: (animation) ? const Duration(seconds: 1) : Duration.zero,
+        child: Container(
+          width: _boxWidth,
+          height: 160,
+          color: Utils.getStatusColor(tokenIssued.statusName),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    boxShadow: const [
+                      BoxShadow(color: Colors.white, spreadRadius: 1),
+                    ],
+                  ),
+                  child: Text(
+                    tokenIssued.statusName.toUpperCase(),
+                    style: const TextStyle(color: Colors.black, fontSize: 12),
+                  ),
                 ),
-                child: Text(
-                  tokenIssued.statusName.toUpperCase(),
-                  style: const TextStyle(color: Colors.black, fontSize: 12),
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                '${tokenIssued.tokenLetter}-${tokenIssued.tokenNumber}',
-                textAlign: TextAlign.center,
-                style: const TextStyle(color: Colors.white, fontSize: 40),
-              ),
-              if (tokenIssued.isOnQueue) ...[
-                const SizedBox(height: 16),
+                const SizedBox(height: 8),
                 Text(
-                  S.of(context).proceedToCounter,
-                  style: const TextStyle(color: Colors.white, fontSize: 24),
+                  '${tokenIssued.tokenLetter}-${tokenIssued.tokenNumber}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white, fontSize: 40),
                 ),
+                if (tokenIssued.isOnQueue) ...[
+                  const SizedBox(height: 16),
+                  Text(
+                    S.of(context).proceedToCounter,
+                    style: const TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ],
               ],
-            ],
+            ),
           ),
         ),
       ),
